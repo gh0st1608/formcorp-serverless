cba4bf7e2d612af59403459830d03cf7616dd9e8
gh0st1608 <egalindoa@uni.pe>
2025-09-20 06:50:59 -0500

    fix

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -4,27 +4,32 @@ resource "aws_api_gateway_rest_api" "this" {
   tags        = var.tags
 }
 
-resource "aws_api_gateway_resource" "claim" {
+resource "aws_api_gateway_resource" "claims" {
   rest_api_id = aws_api_gateway_rest_api.this.id
   parent_id   = aws_api_gateway_rest_api.this.root_resource_id
-  path_part   = "claim"
+  path_part   = "claims"
 }
 
-resource "aws_api_gateway_method" "post" {
+resource "aws_api_gateway_resource" "register" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_resource.claims.id
+  path_part   = "register"
+}
+
+resource "aws_api_gateway_method" "post_register" {
   rest_api_id   = aws_api_gateway_rest_api.this.id
-  resource_id   = aws_api_gateway_resource.claim.id
+  resource_id   = aws_api_gateway_resource.register.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda_proxy" {
+resource "aws_api_gateway_integration" "lambda_proxy_register" {
   rest_api_id             = aws_api_gateway_rest_api.this.id
-  resource_id             = aws_api_gateway_resource.claim.id
-  http_method             = aws_api_gateway_method.post.http_method
+  resource_id             = aws_api_gateway_resource.register.id
+  http_method             = aws_api_gateway_method.post_register.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${var.lambda_arn}/invocations"
-
 }
 
 resource "aws_lambda_permission" "forms_apgw" {
@@ -33,7 +38,6 @@ resource "aws_lambda_permission" "forms_apgw" {
   function_name = var.lambda_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.this.execution_arn}/*/POST/claims/register"
-
 }
 
 resource "aws_api_gateway_deployment" "this" {
