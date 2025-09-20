216b75106f35482ca8709b5cc41375dbaad274c2
gh0st1608 <egalindoa@uni.pe>
2025-09-20 07:00:59 -0500

    deploy fix handler

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -70,7 +70,7 @@ resource "aws_iam_role_policy" "this_policy" {
 resource "aws_lambda_function" "this" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = var.function_name
-  handler          = "index.handler"
+  handler          = "handler.handler"
   runtime          = "nodejs18.x"
   role             = aws_iam_role.this.arn
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
